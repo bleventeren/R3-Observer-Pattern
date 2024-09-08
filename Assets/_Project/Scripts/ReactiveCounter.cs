c23a5fb9b24cfe66e3d26fbf5de5991a7001b0fa
bleventeren <boostlevent@gmail.com>
2024-09-09 00:34:06 +0300

    Project Scripts (Observable)

---
+++ Assets/_Project/Scripts/ReactiveCounter.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using R3;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReactiveCounter : MonoBehaviour
+{
+    public Text counterText;
+    public Button coinButton;
+    public Button cancelButton;
+
+    public Player player;
+    
+    IDisposable subscription;
+    CancellationTokenSource cts;
+
+    private void Awake()
+    {
+        counterText.text = "0";
+        cts = new CancellationTokenSource();
+        cancelButton.onClick.AddListener(() => cts.Cancel());
+    }
+
+    private void Start()
+    {
+        Observable.FromEvent<int>(
+            handler => player.OnPlayerDamaged += handler,
+            handler => player.OnPlayerDamaged -= handler
+        ).Subscribe(
+            damage => Debug.Log($"Player took {damage} damage!")
+        ).AddTo(this);
+        
+        player.CurrentHp.Subscribe(x => Debug.Log("Current HP: " + x)).AddTo(this);
+        player.IsDead.Where(isDead => isDead == true).Subscribe(_ => coinButton.interactable = false).AddTo(this);
+        
+        subscription = coinButton.onClick.AsObservable(cts.Token).Subscribe(_ =>
+        {
+            player.TakeDamage(99); //1
+            counterText.text = (int.Parse(counterText.text) + 1).ToString();
+        });
+    }
+
+    private void OnDestroy()
+    {
+        subscription?.Dispose();
+        cts?.Dispose();
+    }
+}
